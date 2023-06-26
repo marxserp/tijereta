474979704ae3259fc31bce41b6dd36886e734417
marserp2900 <marserp2900@gmail.com>
2023-06-26 19:56:06 -0300

    First commit

---
+++ client/src/pages/HomePage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Let them come</h1>
+    </div>
+  );
+};
+
+export default HomePage;
---
+++ client/src/pages/ReservationForm.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ReservationForm = () => {
+  return (
+    <div>
+      <h1>Make your reservation!</h1>
+    </div>
+  );
+};
+
+export default ReservationForm;
