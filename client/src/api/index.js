56c4134c2fd107380496f372c721662bb52b5097
marxserp <marserp2900@gmail.com>
2024-11-19 13:49:51 -0300

    API cliente ahora apunta a URL correcta

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,13 +1,12 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:8080" });
+const API = axios.create({ baseURL: "https://tijereta-server.onrender.com" });
 
 // Verifiy localStorage !!!
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("profile")) {
-    req.headers.Authorization = `Bearer ${
-      JSON.parse(localStorage.getItem("profile")).token
-    }`;
+    req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem("profile")).token
+      }`;
   }
   return req;
 });
