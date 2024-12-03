61c6d30221831bfe3b0dcc1bb588e5882e5c6ebb
Marcos Serpa <marserp2900@gmail.com>
2024-12-03 15:17:16 -0300

    URL server hardcodeada temporalmente

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: process.env.BASE_URL });
+const API = axios.create({ baseURL: "https://tijereta-server.onrender.com" });
 //const API = axios.create({ baseURL: "http://localhost:8080" });
 
 // Verifiy localStorage !!!
